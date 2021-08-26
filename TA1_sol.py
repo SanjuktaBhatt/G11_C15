da964920a23bd7374b08f58279f03e24b20b4f4b
SanjuktaBhatt <64887786+SanjuktaBhatt@users.noreply.github.com>
2021-08-26 18:37:33 +0530

    Create TA1_sol.py

---
+++ TA1_sol.py
@@ -0,0 +1,8 @@
+#Run the pip command on console
+#pip install git+https://github.+com/ozgur/python-firebase
+
+
+
+from firebase import firebase
+
+firebase = firebase.FirebaseApplication('Database URL', None) #insert url of your database here.Also share the url with the childern.
