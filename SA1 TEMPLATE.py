78e01d82385fb8c4d407b398d10f013ca0eb0490
SanjuktaBhatt <64887786+SanjuktaBhatt@users.noreply.github.com>
2021-08-26 10:11:22 +0530

    Update SA1 TEMPLATE.py

--- SA1 TEMPLATE.py
+++ SA1 TEMPLATE.py
@@ -1,1 +1,9 @@
+from firebase import firebase
+#replace url
+firebase = firebase.FirebaseApplication('database url', None)
+#Create variable for player 1
+
+#Create dictionary for all data of player1
+
+#Upload data to firebase
 
