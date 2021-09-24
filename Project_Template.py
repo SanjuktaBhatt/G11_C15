1dc9f8dc7f6d7dff7f9543b9b578b200ce096173
SanjuktaBhatt <64887786+SanjuktaBhatt@users.noreply.github.com>
2021-09-24 09:42:43 +0530

    Create Project_Template.py

---
+++ Project_Template.py
@@ -0,0 +1,19 @@
+#Use this command in the console to install firebase
+#pip install git+https://github.+com/ozgur/python-firebase
+#import firebase
+from firebase import firebase
+#Connect to to database
+firebase = firebase.FirebaseApplication('Database URL', None)
+
+#create a dictionary of account data
+
+
+
+#Create variable for "name" that holds the value "bank"
+
+#Assign "account_data" to a variable "data"
+
+#Upload data to firebase
+
+
+
